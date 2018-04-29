25da43920049158fd064eec436cee93d9cb721a5
victore07 <victore07@gmail.com>
2018-04-29 02:30:50 -0700

    Improve parser - can parse sentences

--- spaceship_escape/parsing_files/parser.py
+++ spaceship_escape/parsing_files/parser.py
@@ -2,35 +2,86 @@
 # Imports
 from actions import *
 from rooms import *
+from itertools import combinations
+
+# List of words that are relevant to the game
+verbs = ['exit', 'look', 'help', 'inventory', 'savegame', 'loadgame', 'save game', 'load game', 'look at', 'go', 'take', 'pick up', 'grab', 'use']
+items = ['rations', 'map', 'key', 'plant', 'suit', 'extinguisher', 'tools', 'clipboard']
+# Feature list not yet determined
+features = ['door']
+rooms = ['north', 'south', 'east', 'west', 'escape pod', 'loading dock', 'navigation control', 'station control', 'lab', 'energy generation', 'sleeping quarters', 'vr chamber', 'holding chamber', 'maintenance', 'hallway', 'prep chamber', 'space near escape pod', 'space near eva chamber', 'mess']
+
+# Extract essential single and double word commands
+def tokenize(command):
+    # All single and double words
+    single_and_double = [' '.join(word) for word in combinations(command.lower().split(), 2)] + command.lower().split()
+    # Find two word combinations that match keywords
+    tokens = [word for word in single_and_double if word in verbs or word in items or word in features or word in rooms]
+    return tokens
 
 # Dictionary that contains verbs for keys and associated functions as values
 actions = {}
 actions['look'] = look
 actions['help'] = get_verbs
 actions['inventory'] = get_inventory
+actions['savegame'] = save_game
 actions['save game'] = save_game
+actions['loadgame'] = load_game
 actions['load game'] = load_game
 actions['look at'] = look_at
 actions['go'] = go
+actions['take'] = take
+actions['pick up'] = take
+actions['grab'] = take
+actions['use'] = use
 
 # Do action based on input
-def do_action(command, Item=None, Room=None):
-    actions[command](Item, Room)
+def do_action(command, Item=None, Room=None, Feature=None):
+    actions[command](Item, Room, Feature)
 
-# Create room instances
+# Create test room instance
 escape_pod = EscapePod()
-holding_chamber = HoldingChamber()
 
-# Get user input
-command = input('Some action verb: ')
-if command == 'look':
-    location = input('Name of room: ')
+# Parser
+end_game = False
 
-# Call do_action with verb and location
-if command == 'look':
-    if location == 'escape pod':
-        do_action(command, None, escape_pod)
-    elif location == 'holding chamber':
-        do_action(command, None, holding_chamber)
-else:
-    do_action(command)
+# Continue running until command 'exit'
+while not end_game:
+    # Get user input
+    command = input('\ntype a command: ')
+
+    # Tokenized list of key words from command
+    tokens = tokenize(command)
+
+    # Find verb, item, feature, location from list of tokens
+    verb = None
+    item = None
+    feature = None
+    room = None
+
+    for token in tokens:
+        if token in verbs:
+            # Differentitates between 'look' and 'look at'
+            if not verb:
+                verb = token
+        elif token in items:
+            item = token
+        elif token in features:
+            feature = token
+        elif token in rooms:
+            room = token
+
+    print()
+    print(tokens)
+    print('verb:', verb)
+    print('item:', item)
+    print('feature:', feature)
+    print('room:', room, '\n')
+
+    # Call do_action with verb and location
+    if 'exit' in tokens and len(tokens) == 1:
+        end_game = True
+    elif verb:
+        do_action(verb, item, escape_pod, feature)
+    else:
+        print('command not recognized')
