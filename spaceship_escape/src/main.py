fc02d9244c666720d953d27cfe640d509a5cbd33
buteaut <buteaut@gmail.com>
2018-05-06 16:07:41 -0500

    Add files via upload
    main file used to test game.

---
+++ spaceship_escape/src/main.py
@@ -0,0 +1,27 @@
+# File: main.py
+# Author: Reticulum Team
+# Date: 4-19-18
+# Description: This is main file for the Reticulum project.
+
+from game import Game
+#from game import Game
+#from item import Item
+#import platform, os
+
+print("Test test")
+
+#itemTest = Item("Name", "This is the description")
+
+#print("Name test", itemTest.getName())
+
+#print("Description test", itemTest.getDescription())
+
+#itemTest.use()
+
+gameTest = Game()
+gameTest.menu()
+#gameTest.xCoord = 1
+#gameTest.yCoord = 1
+
+#gameTest.travel([1, -1])
+#gameTest.travel([1, 1])
